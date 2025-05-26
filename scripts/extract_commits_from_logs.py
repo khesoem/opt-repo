da1d776155f709f4d778b914d27414d1e8ef4c28
khaes-kth <khaes@kth.se>
2025-05-26 11:44:51 +0200

    commits are further filtered by message

--- scripts/extract_commits_from_logs.py
+++ scripts/extract_commits_from_logs.py
@@ -1,6 +1,9 @@
 with open("../logs/logging_2025-05-25-21-14.log", "r") as file:
     with open('../collected_commits/1000_stars_with_java_modification_and_perf_message.csv', 'w') as output_file:
+        output_file.write("owner,repo,commit_hash,commit_url,commit_message\n")
         perf_mes = None
+        repos = set()
+        owners = set()
         for line in file:
             l = line.strip()
             if 'root INFO' in l:
@@ -11,6 +14,11 @@ with open("../logs/logging_2025-05-25-21-14.log", "r") as file:
                 repo = l.split('/')[-3].strip()
                 perf_mes = perf_mes.replace(',', ' ')
                 perf_mes = '- '.join(perf_mes.split('- ')[1:])
+                if not('perf:' in perf_mes.lower() or 'performance' in perf_mes.lower()):
+                    continue
                 output_file.write(f"{owner},{repo},{commit_hash},{l},{perf_mes}\n")
+                repos.add(f'{owner}/{repo}')
+                owners.add(owner)
             else:
                 perf_mes = perf_mes + " " + l
+        print(f"Collected {len(repos)} repositories and {len(owners)} owners.")
