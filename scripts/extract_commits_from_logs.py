7d314cd79e2923d8715a69bbde68793a6ec84ca1
khaes-kth <khaes@kth.se>
2025-05-26 11:26:21 +0200

    perf commits extracted from logs

---
+++ scripts/extract_commits_from_logs.py
@@ -0,0 +1,16 @@
+with open("../logs/logging_2025-05-25-21-14.log", "r") as file:
+    with open('../collected_commits/1000_stars_with_java_modification_and_perf_message.csv', 'w') as output_file:
+        perf_mes = None
+        for line in file:
+            l = line.strip()
+            if 'root INFO' in l:
+                perf_mes = l.split('root INFO ')[1]
+            elif 'https://' in l and "/commit/" in l:
+                commit_hash = l.split('/')[-1].strip()
+                owner = l.split('/')[-4].strip()
+                repo = l.split('/')[-3].strip()
+                perf_mes = perf_mes.replace(',', ' ')
+                perf_mes = '- '.join(perf_mes.split('- ')[1:])
+                output_file.write(f"{owner},{repo},{commit_hash},{l},{perf_mes}\n")
+            else:
+                perf_mes = perf_mes + " " + l
