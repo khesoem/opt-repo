14caf40721a0426a9e02da39e8eec40e707bd234
Khashayar Etemadi <khesoem@gmail.com>
2025-10-16 22:06:44 +0200

    10 proccesses are used

--- src/config.py
+++ src/config.py
@@ -38,7 +38,7 @@ docker = {
 }
 
 run_analysis = {
-    'num-processes': 2,
+    'num-processes': 10,
     'log-file': 'logs/logging_{:%Y-%m-%d-%H-%M}.log'.format(datetime.now()),
     'log-format': '%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
     'log-datefmt': '%H:%M:%S',
